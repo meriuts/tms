aedcf7e11d132d4b1b1a69a6562e1c05a9336cfa
meriuts <nameriuts@gmail.com>
2024-08-28 15:00:12 +0300

    init

---
+++ src/test/java/ru/tms/task/TmsTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tms.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
